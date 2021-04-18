f005a2f71e6ec586cf4cf3c6cca4ba840368caa0
milosz00 <milosz.czarnik00@gmail.com>
2021-04-18 17:03:22 +0200

    add firebase to app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAU4rSiQiP2zP5jMMm0_MZvCJ9oM9JF3WA",
+    authDomain: "medical-clini.firebaseapp.com",
+    projectId: "medical-clini",
+    storageBucket: "medical-clini.appspot.com",
+    messagingSenderId: "973158807649",
+    appId: "1:973158807649:web:e1e80023e133448338d220",
+    measurementId: "G-BDEYJHB8YY"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAU4rSiQiP2zP5jMMm0_MZvCJ9oM9JF3WA",
+    authDomain: "medical-clini.firebaseapp.com",
+    projectId: "medical-clini",
+    storageBucket: "medical-clini.appspot.com",
+    messagingSenderId: "973158807649",
+    appId: "1:973158807649:web:e1e80023e133448338d220",
+    measurementId: "G-BDEYJHB8YY"
+  },
 };
 
 /*
